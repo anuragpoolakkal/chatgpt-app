df5ae594ac918690825182d510d3ea16f670eeef
Anurag <anuragrajanp@gmail.com>
2023-02-26 22:43:40 +0530

    Update chat_screen.dart

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -105,7 +105,7 @@ class _ChatScreenState extends State<ChatScreen> {
                               chatProvider: chatProvider);
                         },
                         decoration: const InputDecoration.collapsed(
-                            hintText: "How can I help you",
+                            hintText: "How can I help you?",
                             hintStyle: TextStyle(color: Colors.grey)),
                       ),
                     ),
